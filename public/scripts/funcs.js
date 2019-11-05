85b376afd7095d446fa5185806a58b822a01faa2
MasterH12 <heinzosesrunge@gmail.com>
2019-11-05 17:33:28 -0300

    agrega funcionalidad <- inicio de sesion

--- public/scripts/funcs.js
+++ public/scripts/funcs.js
@@ -1,58 +1,100 @@
 //<script src="https://www.gstatic.com/firebasejs/7.2.2/firebase-app.js"></script>
 async function inicializar(){
-	var firebaseConfig = {
-	    apiKey: "AIzaSyAmTGCzHl7-SxeNf8q81Nt01n67vjqfv0U",
-	    authDomain: "cesfam-francia.firebaseapp.com",
-	    databaseURL: "https://cesfam-francia.firebaseio.com",
+	// Your web app's Firebase configuration
+	const firebaseConfig = {
+	  	apiKey: "AIzaSyAmTGCzHl7-SxeNf8q81Nt01n67vjqfv0U",
+	  	authDomain: "cesfam-francia.firebaseapp.com",
+	  	databaseURL: "https://cesfam-francia.firebaseio.com",
 	    projectId: "cesfam-francia",
 	    storageBucket: "cesfam-francia.appspot.com",
 	    messagingSenderId: "1098474308171",
 	    appId: "1:1098474308171:web:ce3d547564502df8f3c3ce"
 	};
-	firebase.initializeApp(firebaseConfig);
-	firebase.auth().onAuthStateChanged( async function(user) {
-		if (user) {
-		  	var user = firebase.auth().currentUser;
-		  	await firebase.auth().currentUser.getIdToken(/* forceRefresh */ true).then(function(idToken) {
-		  		var validado= true;
-		  		if(idToken===null){
-		  			window.location.href="../index.html";
-		  		}
-			}).catch(function(error) {
-				window.location.href="../index.html";
-			});
+	// Initialize Firebase
+	await firebase.initializeApp(firebaseConfig);
 
-			await firebase.firestore().collection("Usuarios").where("nombre","==",user.displayName).get()
-			.then(function(querySnapshot) {
-				var validado= true;
-			});
-		}
-		else{
-			window.location.href="../index.html";
-		}
+	await firebase.auth().onAuthStateChanged( async function(user) {
+	    if (user){
+	    	await firebase.auth().currentUser.getIdToken(/* forceRefresh */ true).then(function(idToken) {
+		        if(idToken!=null){
+		        	setTimeout("despliegue()", 1000);
+		        } else{
+		        	document.getElementById("loading").innerHTML = "";
+		        }
+		    });
+	    }else{
+	    	document.getElementById("loading").innerHTML = "";	
+	    }
+	    
+	});
+}
+
+function loginF(){
+	document.getElementById("loading").innerHTML = "<div class='spinner-border' role='status' style='color:#00c0b7'><span class='sr-only'>Loading...</span></div><p style='color:#00c0b7'><small>comprobando sesión</small></p>";
+	var entrar=true;
+	var p = document.forms["login"]["pass"].value.toString();
+    var u = document.forms["login"]["user"].value.toString();
+	var aviso=document.getElementById("aviso");
+
+    firebase.auth().signInWithEmailAndPassword(u, p).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        entrar=false;
+        //errorCode == "auth/wrong-password"
+        if(errorCode=="auth/too-many-requests"){
+            aviso.innerHTML="Has intentado muchas veces, vuelve a inentarlo más tarde";
+            document.getElementById("loading").innerHTML ="";
+        }
+        else{
+            aviso.innerHTML="Contraseña y/o correo electrónico inválidos";
+            document.getElementById("loading").innerHTML ="";
+        }
+      // ...
+    }).then(function(resultado){
+        if(entrar){
+            firebase.auth().onAuthStateChanged(function(user) {
+                if (user) {
+                    aviso.innerHTML="¡sesión iniciada!";
+                    setTimeout("despliegue()", 1000);
+                    document.getElementById("loading").innerHTML ="";
+                }
+            });
+        }
+    });
+    return false;
+}
+
+function sacar(){
+    window.location.href="login.html";
+}
+
+function despliegue(u, p, pag){
+	document.getElementById("content").innerHTML="<div id='informacion'></div>";
+	document.getElementById("informacion").innerHTML = "<button type='button' class='btn btn-outline-primary' style='float:right' onClick='logOut()'>Log Out</button>";
+	var usuario = new Usuario();
+}
+
+async function logOut(){
+	firebase.auth().signOut().then(function(){
+		document.getElementById("content").innerHTML="deslogueado";
 	});
 }
 class Visita{
 	constructor(){
-		this.user;	
+		this.especialidades;
+		this.eventos;
 	}
 	
 }
 class Usuario{
-}
-class Admin extends Usuario{
 	constructor(){
-		super();
+		this.user;
+		this.especialidades;
+		this.eventos;
+		this.reuniones;
 	}
-}
+	obtenerUsuario(user, pass){
 
-function loginF(){
-	var p = document.forms["login"]["pass"].value.toString();
-    var u = document.forms["login"]["user"].value.toString();
-
-    if(p=="admin" && u=="admin@correo"){
-    	document.getElementById("aviso").innerHTML = "conectado";
-    	document.getElementById("aviso").style.class = "text-success";
-    }
-    return false;
+	}
 }
