7be6c16ed1790809f24f33409bb504f26301f306
MasterH12 <heinzosesrunge@gmail.com>
2019-11-07 00:55:49 -0300

    agrega intranet <- calendario

--- public/scripts/funcs.js
+++ public/scripts/funcs.js
@@ -14,14 +14,19 @@ async function inicializar(){
 	await firebase.initializeApp(firebaseConfig);
 
 	await firebase.auth().onAuthStateChanged( async function(user) {
+		var token=true;
 	    if (user){
-	    	await firebase.auth().currentUser.getIdToken(/* forceRefresh */ true).then(function(idToken) {
-		        if(idToken!=null){
-		        	setTimeout("despliegue()", 1000);
-		        } else{
+	    	await firebase.auth().currentUser.getIdToken(true).then(function(idToken) {
+		        if(idToken===null){
 		        	document.getElementById("loading").innerHTML = "";
+		        	token=false;
 		        }
 		    });
+		    if(token){
+		    	setTimeout(function(){
+		    		despliegue(firebase.auth().currentUser,"intranet");
+		    	}, 1000);	
+		    }
 	    }else{
 	    	document.getElementById("loading").innerHTML = "";	
 	    }
@@ -30,7 +35,7 @@ async function inicializar(){
 }
 
 function loginF(){
-	document.getElementById("loading").innerHTML = "<div class='spinner-border' role='status' style='color:#00c0b7'><span class='sr-only'>Loading...</span></div><p style='color:#00c0b7'><small>comprobando sesión</small></p>";
+	document.getElementById("loading").innerHTML = "<div class='spinner-border' role='status' style='color:#00c0b7'><span class='sr-only'>Loading...</span></div><p style='color:#00c0b7'><small>iniciando sesión</small></p>";
 	var entrar=true;
 	var p = document.forms["login"]["pass"].value.toString();
     var u = document.forms["login"]["user"].value.toString();
@@ -69,15 +74,18 @@ function sacar(){
     window.location.href="login.html";
 }
 
-function despliegue(u, p, pag){
+async function despliegue(u, pag){
 	document.getElementById("content").innerHTML="<div id='informacion'></div>";
-	document.getElementById("informacion").innerHTML = "<button type='button' class='btn btn-outline-primary' style='float:right' onClick='logOut()'>Log Out</button>";
 	var usuario = new Usuario();
+	await usuario.obtenerUsuario(u.email);
+	document.getElementById("informacion").innerHTML = "<button type='button' class='btn btn-outline-primary' style='float:right;' onClick='logOut()'>Log Out</button>" + usuario.desplegarUsuario() + "<div id='calendar'></div>";
+	usuario.desplegarCalendario();
 }
 
 async function logOut(){
+	document.getElementById("content").innerHTML="<div align='center' style='width:100%;height100%;vertical-align:middle'><div class='spinner-border' role='status' style='color:#00c0b7'><span class='sr-only'>Loading...</span></div><p style='color:#00c0b7'><small>cerrando sesión</small></p><div>";;
 	firebase.auth().signOut().then(function(){
-		document.getElementById("content").innerHTML="deslogueado";
+		document.getElementById("content").innerHTML=desplegarLogin();
 	});
 }
 class Visita{
@@ -87,6 +95,35 @@ class Visita{
 	}
 	
 }
+function desplegarLogin(){
+	var str = "";
+	str = str + "<div style='padding-left:25%;padding-right: 25%;padding-top:5%;padding-bottom:5%'>";
+	str = str + "<form onsubmit='return loginF()' name='login'>";
+	str = str + "<div class='form-group'>";
+	str = str + "<label for='exampleInputEmail1'>Correo Electrónico</label>";
+	str = str + "<input type='email' name='user' class='form-control' id='exampleInputEmail1' aria-describedby='emailHelp' placeholder='ejemplo@correo.com' required/>";
+	str = str + "</div>";
+	str = str + "<div class='form-group'>";
+	str = str + "<label for='exampleInputPassword1'>Contraseña</label>";
+	str = str + "<input type='password' name='pass' class='form-control' id='exampleInputPassword1' placeholder='Contraseña' required/>";
+	str = str + "</div>"; 		   
+	str = str + "<p id='aviso' class='text-success' style='width:320px;border-radius: 5px;'></p>";
+	str = str + "<div class='form-group form-check'>";
+	str = str + "<input type='checkbox' class='form-check-input' id='exampleCheck1'>";
+	str = str + "<label class='form-check-label' for='exampleCheck1'>Mantenerse conectado</label>";
+	str = str + "</div>";
+	str = str + "<div align='center' id='loading'>";
+	str = str + "<div class='spinner-border' role='status' style='color:#00c0b7'>";
+	str = str + "<span class='sr-only'>Loading...</span>";
+	str = str + "</div>";
+	str = str + "<p style='color:#00c0b7'><small>comprobando sesión</small></p>";
+	str = str + "</div>";
+	str = str + "<button type='submit' class='btn btn-primary'>Entrar</button>";
+	str = str + "</form>";
+	str = str + "</div>";
+	str = str + "</div>";
+	return str;
+}
 class Usuario{
 	constructor(){
 		this.user;
@@ -94,7 +131,254 @@ class Usuario{
 		this.eventos;
 		this.reuniones;
 	}
-	obtenerUsuario(user, pass){
+	async obtenerUsuario(email){
+		var usuario= new Object();
+		usuario.correo = email;
+		await firebase.firestore().collection("Usuarios").where("correo","==",email).get()
+		.then(function(querySnapshot) {
+			const data = querySnapshot.docs[0].data();
+			usuario.admin = data.cargo;
+			usuario.cecosf = data.cecosf;
+			usuario.especialidad = data.especialidad;
+			usuario.nombre = data.nombre;
+		});
+		this.user = usuario;
 
 	}
+	desplegarUsuario(){
+		var str = "<div class='row'>";
+		str = str + "<div class='col'>";
+		str = str + "Nombre: " + this.user.nombre;
+		str = str + "<br>";
+		str = str + "Establecimiento: " + establecimiento(this.user.cecosf);		
+		str = str + "<br>";
+		str = str + "Especialidad: " + this.user.especialidad;
+		str = str + "<br>";
+		str = str + "Correo: " + this.user.correo;
+		if(this.user.admin){
+			str = str + "<h4 style='color:#00c0b7'>Permisos de administrador concedidos<h4>";	
+		}
+		str = str + "<br>";
+		str = str + "</div>";
+		str = str + "</div>";
+		return str;
+	}
+	desplegarCalendario(){
+		if(this.user.admin){
+			var calendarEl = document.getElementById('calendar');
+
+	        var calendar = new FullCalendar.Calendar(calendarEl, {
+	          plugins: [ 'interaction', 'dayGrid' ],
+	          defaultDate: '2019-08-12',
+	          locale:'es',
+	          navLinks: false, // can click day/week names to navigate views
+
+		      weekNumberCalculation: 'ISO',
+		      editable:true,
+		      // CUANDO SE PUEDA EDITAR, GUARDAR EL DOC_ID DE CADA EVENTO, PARA QUE CUANDO SEAN MODIFICADOS, PUEDAN SER GUARDADOS EN SUS DOC_ID'S CORRESPONDIENTES
+		      eventLimit: true,
+	          events: [
+		        {
+		          title: 'Business Lunch',
+		          start: '2019-08-03T13:00:00',
+		          constraint: 'businessHours'
+		        },
+		        {
+		          title: 'Meeting',
+		          start: '2019-08-13T11:00:00',
+		          constraint: 'availableForMeeting', // defined below
+		          color: '#257e4a',
+		          description: 'Reunión de avances',
+		          organiza: 'a',
+		          tematica: 'psicología',
+		        },
+		        {
+		          title: 'Conference',
+		          start: '2019-08-18',
+		          end: '2019-08-20'
+		        },
+		        {
+		          title: 'Party',
+		          start: '2019-08-29T20:00:00'
+		        },
+
+		        // areas where "Meeting" must be dropped
+		        {
+		          groupId: 'availableForMeeting',
+		          start: '2019-08-11T10:00:00',
+		          end: '2019-08-11T16:00:00',
+		          rendering: 'background'
+		        },
+		        {
+		          groupId: 'availableForMeeting',
+		          start: '2019-08-13T10:00:00',
+		          end: '2019-08-13T16:00:00',
+		          rendering: 'background'
+		        },
+
+		        // red areas where no events can be dropped
+		        {
+		          start: '2019-08-24',
+		          end: '2019-08-28',
+		          overlap: false,
+		          rendering: 'background',
+		          color: '#ff9f89'
+		        },
+		        {
+		          start: '2019-08-06',
+		          end: '2019-08-08',
+		          overlap: false,
+		          rendering: 'background',
+		          color: '#ff9f89'
+		        }
+		      ],
+		      eventClick: function(info) {
+		        $('#evento').modal('show');
+		        var evento = new Object();
+		        evento.titulo = info.event.title;
+		        evento.fecha = info.event.start.toLocaleDateString();
+		        evento.tematica = info.event.extendedProps.tematica;
+		        evento.descripcion = info.event.extendedProps.description;
+		        evento.organiza = establecimiento(info.event.extendedProps.organiza)
+		        document.getElementById("evento").innerHTML = desplegarEvento(evento);
+		      }
+	        });
+		} else{
+			var calendarEl = document.getElementById('calendar');
+
+	        var calendar = new FullCalendar.Calendar(calendarEl, {
+	          plugins: ['dayGrid' ],
+	          defaultDate: '2019-08-12',
+	          locale:'es',
+	          navLinks: false, // can click day/week names to navigate views
+
+		      weekNumberCalculation: 'ISO',
+		      editable:true,
+		      // CUANDO SE PUEDA EDITAR, GUARDAR EL DOC_ID DE CADA EVENTO, PARA QUE CUANDO SEAN MODIFICADOS, PUEDAN SER GUARDADOS EN SUS DOC_ID'S CORRESPONDIENTES
+		      eventLimit: true,
+	          events: [
+		        {
+		          title: 'Business Lunch',
+		          start: '2019-08-03T13:00:00',
+		          constraint: 'businessHours'
+		        },
+		        {
+		          title: 'Meeting',
+		          start: '2019-08-13T11:00:00',
+		          constraint: 'availableForMeeting', // defined below
+		          color: '#257e4a',
+		          description: 'Reunión de avances',
+		          organiza: 'a',
+		          tematica: 'psicología',
+		        },
+		        {
+		          title: 'Conference',
+		          start: '2019-08-18',
+		          end: '2019-08-20'
+		        },
+		        {
+		          title: 'Party',
+		          start: '2019-08-29T20:00:00'
+		        },
+
+		        // areas where "Meeting" must be dropped
+		        {
+		          groupId: 'availableForMeeting',
+		          start: '2019-08-11T10:00:00',
+		          end: '2019-08-11T16:00:00',
+		          rendering: 'background'
+		        },
+		        {
+		          groupId: 'availableForMeeting',
+		          start: '2019-08-13T10:00:00',
+		          end: '2019-08-13T16:00:00',
+		          rendering: 'background'
+		        },
+
+		        // red areas where no events can be dropped
+		        {
+		          start: '2019-08-24',
+		          end: '2019-08-28',
+		          overlap: false,
+		          rendering: 'background',
+		          color: '#ff9f89'
+		        },
+		        {
+		          start: '2019-08-06',
+		          end: '2019-08-08',
+		          overlap: false,
+		          rendering: 'background',
+		          color: '#ff9f89'
+		        }
+		      ],
+		      eventClick: function(info) {
+		        $('#evento').modal('show');
+		        
+		        var evento = new Object();
+		        evento.titulo = info.event.title;
+		        evento.fecha = info.event.start.toLocaleDateString();
+		        evento.tematica = info.event.extendedProps.tematica;
+		        evento.descripcion = info.event.extendedProps.description;
+		        evento.organiza = establecimiento(info.event.extendedProps.organiza)
+
+		        document.getElementById("tituloEvento").innerHTML = evento.titulo;
+		        document.getElementById("fechaEvento").innerHTML = evento.fecha;
+		        document.getElementById("tematica").innerHTML = evento.tematica;
+		        document.getElementById("descripcionEvento").innerHTML = evento.descripcion;
+		        document.getElementById("organiza").innerHTML = evento.organiza;
+		      }
+	        });
+		}
+        
+
+        calendar.render();
+	}
+}
+function establecimiento(palabra){
+	switch (palabra){
+		case 'a':
+			return "CESFAM Francia";
+		case 'b':
+			return "CECOSF Gral Yañez";
+		case 'c':
+			 return"CECOSF Sergio Santander 4027";
+		case 'd':
+			return "CECOSF Arauco 709";
+		case 'e':
+			return "CECOSF Las Mulatas 311";
+		case 'f':
+			return "CECOSF Caupolican 201";
+		case 'g':
+			return "CECOSF Arauco 810";
+	}
+}
+function desplegarEvento(evento){
+	var str = "<div class='modal-dialog' role='document'>";
+	str = str + "<div class='modal-content'>";
+	str = str + "<div class='modal-header' style='display: block'>";
+	str = str + "<div class='row'>";
+	str = str + "<div class='col'>";
+	str = str + "<h5 class='modal-title' id='tituloEvento'>" + evento.titulo + "</h5>";
+	str = str + "</div>";
+	str = str + "<div class='col' align='right'>";
+	str = str + "<button type='button' class='btn btn-outline-danger'>Eliminar</button>";
+	str = str + "</div>";
+	str = str + "</div>";
+	str = str + "<div class='row'>";
+	str = str + "<div class='col'>";			
+	str = str + "<h6 class='modal-title' id='fechaEvento'>" + evento.fecha + "</h6>";				
+	str = str + "</div>";
+	str = str + "</div>";
+	str = str + "</div>";
+	str = str + "<div class='modal-body'>";
+	str = str + "<h6 id='tematica'>" + evento.tematica + "</h6>";		
+	str = str + "<p id='descripcionEvento'>" + evento.descripcion + "</p>";
+	str = str + "<h6 id='organiza' align='center'>" + evento.organiza + "</h6>";
+	str = str + "</div>";
+	str = str + "<div class='modal-footer'>";
+	str = str + "<button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button>";
+	str = str + "</div>";
+	str = str + "</div>";
+	str = str + "</div>";
+	return str;
 }
